1c766b2d3c17e6c1de5f03b6e0fdec268bdfffba
Mallika-2002 <98608157+Mallika-2002@users.noreply.github.com>
2023-06-23 12:36:39 +0530

    Update AmodB.java

--- leetcode/AmodB.java
+++ leetcode/AmodB.java
@@ -1,5 +1,13 @@
-package leetcode;
-
-public class AmodB {
-
+import java.util.*;
+class Solution {
+    public int superPow(int a, int[] b) {
+        String st ="";
+        for(int x:b)
+        st= st + Integer.toString(x);
+     java.math.BigInteger p= new java.math.BigInteger(Integer.toString(a));
+      java.math.BigInteger q = new java.math.BigInteger(st);
+       java.math.BigInteger r = new java.math.BigInteger("1337");
+        java.math.BigInteger res = p.modPow(q,r);
+     return res.intValue();
+    }
 }
