05269708d666f73f2b15d19c6b0ad1d7bd4ae601
Mallika-2002 <98608157+Mallika-2002@users.noreply.github.com>
2023-06-23 12:35:47 +0530

    Update PowerOfThree.java

--- leetcode/PowerOfThree.java
+++ leetcode/PowerOfThree.java
@@ -1,5 +1,9 @@
-package leetcode;
-
-public class PowerOfThree {
-
+class Solution {
+    public boolean isPowerOfThree(int n) {
+     if(n==1)
+     return true;
+     if(n%3!=0||n<=0)
+     return false;
+     return isPowerOfThree(n/3);
+    }
 }
